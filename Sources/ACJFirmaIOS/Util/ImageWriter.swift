431e9a89685ac389a4aae2bf88bfb6df715ed993
MatFragg <u202318323@upc.edu.pe>
2026-07-27 10:11:02 -0500

    Fix ImageWriter: add explicit closure type annotation to help Swift 6 type checker

--- Sources/ACJFirmaIOS/Util/ImageWriter.swift
+++ Sources/ACJFirmaIOS/Util/ImageWriter.swift
@@ -20,10 +20,11 @@ public class ImageWriter {
         let scaledHeight = CGFloat(height) * scale
 
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: scaledWidth, height: scaledHeight))
-        let image = renderer.image { ctx in
-            ctx.cgContext.scaleBy(x: scale, y: scale)
-            ctx.cgContext.setFillColor(UIColor.white.cgColor)
-            ctx.cgContext.fill(CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
+        let image = renderer.image { (ctx: UIGraphicsImageRendererContext) -> Void in
+            let c = ctx.cgContext
+            c.scaleBy(x: scale, y: scale)
+            c.setFillColor(UIColor.white.cgColor)
+            c.fill(CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
 
             let logoAreaWidth = CGFloat(width) * 0.30
             let paddingX: CGFloat = 4
@@ -39,28 +40,25 @@ public class ImageWriter {
                 }
             }
 
-            let textoEnvolvente = SignHelpers.cropText(nombreFinal, longitud: 30)
-            let lineasNombre = textoEnvolvente.components(separatedBy: "\n")
-            let lineasCargo: [String]
+            let envolvente = SignHelpers.cropText(nombreFinal, longitud: 30)
+            let lineasNombre = envolvente.components(separatedBy: "\n")
+
+            var lineasCargo: [String] = []
             if let c = cargo, !c.isEmpty {
                 lineasCargo = SignHelpers.cropText(c, longitud: 30).components(separatedBy: "\n")
-            } else {
-                lineasCargo = []
             }
-            let lineasEmpresa: [String]
+            var lineasEmpresa: [String] = []
             if let e = empresa, !e.isEmpty {
                 lineasEmpresa = SignHelpers.cropText(e, longitud: 30).components(separatedBy: "\n")
-            } else {
-                lineasEmpresa = []
             }
 
-            let totalContentHeight = lineH * CGFloat(1 + lineasNombre.count + lineasCargo.count + lineasEmpresa.count + 1 + 1)
-            var yStart = (CGFloat(height) - totalContentHeight) / 2 + CGFloat(fontSize) - 1
+            let lineCount = 1 + lineasNombre.count + lineasCargo.count + lineasEmpresa.count + 1 + 1
+            let totalContentHeight = lineH * CGFloat(lineCount)
+            let yStart = (CGFloat(height) - totalContentHeight) / 2 + CGFloat(fontSize) - 1
 
             let paragraphStyle = NSMutableParagraphStyle()
             paragraphStyle.lineBreakMode = .byWordWrapping
-
-            let textAttributes: [NSAttributedString.Key: Any] = [
+            let textAttrs: [NSAttributedString.Key: Any] = [
                 .font: UIFont.systemFont(ofSize: CGFloat(fontSize)),
                 .foregroundColor: UIColor.black,
                 .paragraphStyle: paragraphStyle,
@@ -71,42 +69,37 @@ public class ImageWriter {
                 let aspectRatio = logoImg.size.height / logoImg.size.width
                 var logoDrawWidth = logoAvailableWidth
                 var logoDrawHeight = logoDrawWidth * aspectRatio
-
                 if logoDrawHeight > CGFloat(height) - 10 {
                     logoDrawHeight = CGFloat(height) - 10
                     logoDrawWidth = logoDrawHeight / aspectRatio
                 }
-
                 let logoX = paddingX + (logoAvailableWidth - logoDrawWidth) / 2
                 let logoY = (CGFloat(height) - logoDrawHeight) / 2
                 logoImg.draw(in: CGRect(x: logoX, y: logoY, width: logoDrawWidth, height: logoDrawHeight))
             }
 
-            var currentY = yStart
+            var y = yStart
             let displayTitulo = titulo.isEmpty ? "Firmado digitalmente por" : titulo
-            (displayTitulo as NSString).draw(at: CGPoint(x: xTexto, y: currentY), withAttributes: textAttributes)
+            (displayTitulo as NSString).draw(at: CGPoint(x: xTexto, y: y), withAttributes: textAttrs)
+            y += lineH
 
-            currentY += lineH
             for linea in lineasNombre {
-                (linea as NSString).draw(at: CGPoint(x: xTexto, y: currentY), withAttributes: textAttributes)
-                currentY += lineH
+                (linea as NSString).draw(at: CGPoint(x: xTexto, y: y), withAttributes: textAttrs)
+                y += lineH
             }
-
             for linea in lineasEmpresa {
-                (linea as NSString).draw(at: CGPoint(x: xTexto, y: currentY), withAttributes: textAttributes)
-                currentY += lineH
+                (linea as NSString).draw(at: CGPoint(x: xTexto, y: y), withAttributes: textAttrs)
+                y += lineH
             }
-
             for linea in lineasCargo {
-                (linea as NSString).draw(at: CGPoint(x: xTexto, y: currentY), withAttributes: textAttributes)
-                currentY += lineH
+                (linea as NSString).draw(at: CGPoint(x: xTexto, y: y), withAttributes: textAttrs)
+                y += lineH
             }
 
             let fechaDisplay = fecha.hasPrefix("Fecha") ? fecha : "Fecha: \(fecha)"
-            (fechaDisplay as NSString).draw(at: CGPoint(x: xTexto, y: currentY), withAttributes: textAttributes)
-            currentY += lineH
-
-            ("Firmado con ACJ Signature" as NSString).draw(at: CGPoint(x: xTexto, y: currentY), withAttributes: textAttributes)
+            (fechaDisplay as NSString).draw(at: CGPoint(x: xTexto, y: y), withAttributes: textAttrs)
+            y += lineH
+            ("Firmado con ACJ Signature" as NSString).draw(at: CGPoint(x: xTexto, y: y), withAttributes: textAttrs)
         }
         return image
     }
